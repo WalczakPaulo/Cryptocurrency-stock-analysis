1b07e1fd4221a97136d35fef688fcdfa5ea0c907
Igor Markiewicz <igomar067@gmail.com>
2019-01-13 15:21:30 +0100

    Adding additional data processing steps

--- Modules/DataAnalysis/NLPModel.py
+++ Modules/DataAnalysis/NLPModel.py
@@ -2,23 +2,38 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from gensim.models.doc2vec import TaggedDocument
 from gensim.utils import simple_preprocess
-from gensim.parsing.preprocessing import remove_stopwords
 from gensim.models.doc2vec import Doc2Vec
+from nltk.corpus import stopwords
+from nltk.stem import SnowballStemmer
 
-def to_tagged_doc(doc):
+
+def to_tagged_doc(doc, additional_stop_words=None):
+    additional_stop_words = additional_stop_words if additional_stop_words else {}
+    sno = SnowballStemmer('english')
     pd.options.mode.chained_assignment = None
-    doc['messages'] = doc['messages'].map(remove_stopwords)
-    return doc.apply(lambda x: TaggedDocument(words=simple_preprocess(x['messages']), tags=[x['class']]), axis=1)
+    stop_words = set(stopwords.words('english')).union(additional_stop_words)
+    processed = doc['messages'].map(lambda x: simple_preprocess(x))
+    processed = processed.map(lambda x: map(sno.stem, x))
+    processed = processed.map(lambda x: list(filter(lambda s: s not in stop_words, x)))
 
-def read_and_preprocess(path='./data/ProcessedData.csv', is_training=False, test_size=0.2, random_state=None):
+    return [TaggedDocument(words=w, tags=[c]) for w, c in zip(processed, doc['class'])]
+
+def read_and_preprocess(path='./data/ProcessedData.csv', is_training=False, test_size=0.2,
+                        additional_stop_words=None, random_state=None):
+    additional_stop_words = additional_stop_words if additional_stop_words else {}
     df = pd.read_csv(path)
     df.drop(['date'], axis=1, inplace=True)
-    to_tagged_doc(df)
     if is_training:
         train, test = train_test_split(df, test_size=test_size, random_state=random_state)
-        return to_tagged_doc(train), to_tagged_doc(test)
+
+        return to_tagged_doc(train, additional_stop_words), to_tagged_doc(test, additional_stop_words)
     else:
-        return [simple_preprocess(x) for x in df['messages']]
+        stop_words = set(stopwords.words('english')).union(additional_stop_words)
+        sno = SnowballStemmer('english')
+        processed = [simple_preprocess(x) for x in df['messages']]
+        processed = [[sno.stem(x) for x in doc] for doc in processed]
+
+        return [list(filter(lambda x: x not in stop_words, doc)) for doc in processed]
 
 def infered_vectors(model, messages, steps=100, alpha=0.025, min_alpha=0.01):
     return zip(*[(model.infer_vector(doc.words, steps=steps, alpha=alpha,
@@ -29,17 +44,20 @@ def build_model(path_to_data='./data/ProcessedData.csv',
                 path_to_model_dir='./model/nlp.model',
                 test_size=0.2,
                 split_data_random_state=None,
+                additional_stop_words=None,
                 **params):
 
+    additional_stop_words = additional_stop_words if additional_stop_words else {}
     train_tagged, test_tagged = read_and_preprocess(path=path_to_data, test_size=test_size, is_training=True,
+                                                    additional_stop_words=additional_stop_words,
                                                     random_state=split_data_random_state)
-    train_messages = train_tagged.values
-    model_PVDBOW = Doc2Vec(train_messages, dm=0, vector_size=params['vector_size'], window=params['window'],
+
+    model_PVDBOW = Doc2Vec(train_tagged, dm=0, vector_size=params['vector_size'], window=params['window'],
                            min_count=params['min_count'], epochs=params['epochs'], alpha=params['alpha'],
                            min_alpha=params['min_alpha'])
-    Xx, yy = infered_vectors(model_PVDBOW, train_messages)
-    test_messages = test_tagged.values
-    Xt, yt = infered_vectors(model_PVDBOW, test_messages)
+
+    Xx, yy = infered_vectors(model_PVDBOW, train_tagged)
+    Xt, yt = infered_vectors(model_PVDBOW, test_tagged)
 
     if is_model_saved:
         model_PVDBOW.save(path_to_model_dir)
--- Modules/DataAnalysis/modelTests.py
+++ Modules/DataAnalysis/modelTests.py
@@ -3,9 +3,10 @@ from sklearn import svm
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import f1_score
 from NLPModel import build_model
+from NLPModel import read_and_preprocess
 
-Xx, yy, Xt, yt, model_PVDBOW = build_model(vector_size=300, window=10, min_count=1, epochs=100, alpha=0.025,
-                           min_alpha=0.01)
+Xx, yy, Xt, yt, model_PVDBOW = build_model(additional_stop_words={'sbquo', 'www', 'http', 'com'}, vector_size=300,
+                                           window=10, min_count=2, epochs=100, alpha=0.025, min_alpha=0.01)
 
 logistic_regression = LogisticRegression().fit(Xx, yy)
 y_pred_lr = logistic_regression.predict(Xt)
@@ -19,3 +20,5 @@ print('logistic regression accuracy = {} | svm accuracy = {}\n'
               accuracy_score(yt, y_pred_svm),
               f1_score(yt, y_pred_lr, average="macro"),
               f1_score(yt, y_pred_svm, average="macro")))
+
+# print(read_and_preprocess(is_training=False, additional_stop_words={'sbquo', 'www', 'http', 'com'}))
